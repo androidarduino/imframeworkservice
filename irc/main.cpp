81cd747913cd3e7acd012053bd350bb3d6b90de6
vrcats <vrcats@79cadf2e-45d3-11de-a1df-459bf3fe8569>
2010-11-26 05:36:25 +0000

    added adapter classes

--- irc/main.cpp
+++ irc/main.cpp
@@ -1,11 +1,14 @@
 #include "irc.h"
 #include <QApplication>
 
+using namespace irc;
+
 int main(int argc, char** argv)
 {
     QApplication app(argc, argv);
     IRCClient client("irc.nokia.troll.no", 6667);
     client.d_userName="vrcats";
+    client.d_realName="zheng Liu";
     client.connect();
     client.join("#chinese");
     return app.exec();
