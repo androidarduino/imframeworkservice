1531bf8641b6f5a108fb2f0e703ce3370ae5e866
vrcats <vrcats@79cadf2e-45d3-11de-a1df-459bf3fe8569>
2010-12-03 05:45:16 +0000

    added todo

---
+++ adapter/interface.h
@@ -0,0 +1,34 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+/*
+    In this file a set of interface classes were defined.
+    These interface classes provide developer the ability of communicating to other parties easily and freely.
+*/
+
+namespace IMFramework
+{
+
+class Presence
+{
+
+};
+
+class Messenger
+{
+
+};
+
+class SyncAble
+{
+
+};
+
+class Query
+{
+
+};
+
+}
+
+#endif
