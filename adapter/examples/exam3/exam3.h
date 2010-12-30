612c2dc313f3f6c74133456be8e854d6bc9530db
vrcats <vrcats@79cadf2e-45d3-11de-a1df-459bf3fe8569>
2010-12-30 06:39:02 +0000

    added example files

---
+++ adapter/examples/exam3/exam3.h
@@ -0,0 +1,39 @@
+#ifndef EXAM3_H
+#define EXAM3_H
+
+//#include "../../interface.h"
+#include <QApplication>
+#include <QtGui>
+#include <QString>
+#include <QRegExp>
+#include <QDebug>
+#include <iostream>
+#include <string>
+/*
+    This example is to demonstrate how to use the Query class.
+    In this example, we implemented a sql query machine. User can send a query to a remote machine and get the result asap.
+   */
+
+//this class sends the question
+class QuerySender: public QObject
+{
+    Q_OBJECT
+    public:
+        QuerySender();
+        void linkTo(Group server);
+        void query(QString sqlQuery, QString format="");
+};
+
+//this class answers the question with preferred format
+class QueryServer: public QObject
+{
+    Q_OBJECT
+    public:
+        QueryServer();
+    public slots:
+        void gotQuery(QString sqlQuery, QString format);
+    private:
+        void answer(Group receiver, QVariant results);
+};
+
+#endif
