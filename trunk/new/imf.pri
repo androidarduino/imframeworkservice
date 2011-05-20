240a5922c9caed0260cbc6e96e973f1843129f60
vrcats <vrcats@79cadf2e-45d3-11de-a1df-459bf3fe8569>
2011-05-20 07:19:42 +0000

    created some pro files, irc pro is a good example to follow

---
+++ trunk/new/imf.pri
@@ -0,0 +1,6 @@
+# Common definitions
+
+IMF_VERSION = 0.1
+QXMPP_INCLUDE_DIR = $$PWD/src
+QXMPP_LIBRARY_DIR = $$PWD/lib
+
--- trunk/new/src/protocols/irc/main.cpp
+++ trunk/new/src/protocols/irc/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
     QApplication app(argc, argv);
     IRCClient client("irc.nokia.troll.no", 6667);
     client.d_userName="vrcats";
-    client.d_realName="zheng Liu";
+    client.d_realName="zliu";
     client.connect();
     client.join("#chinese");
     return app.exec();
