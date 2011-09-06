b4bc8a584edf51655272035d64af36fbc97181ac
vrcats <vrcats@79cadf2e-45d3-11de-a1df-459bf3fe8569>
2011-09-06 08:41:25 +0000

    added

--- src/service/improtocols.h
+++ src/service/improtocols.h
@@ -1,8 +1,8 @@
 #ifndef IMPROTOCOLS_H
 #define IMPROTOCOLS_H
-#include "../irc/irc.h"
-#include "../xmpp/src/QXmppClient.h"
-#include "../xmpp/src/QXmppMessage.h"
-#include "../xmpp/src/QXmppRosterManager.h"
+#include "irc/irc.h"
+#include "xmpp/src/QXmppClient.h"
+#include "xmpp/src/QXmppMessage.h"
+#include "xmpp/src/QXmppRosterManager.h"
 //#include "../msn/msn.h"
 #endif
