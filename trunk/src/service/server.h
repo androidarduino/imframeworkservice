c538f34ef2dcd8e53c726c07c2bddb24bb203a8b
vrcats <vrcats@79cadf2e-45d3-11de-a1df-459bf3fe8569>
2011-09-06 08:41:25 +0000

    added

---
+++ trunk/src/service/server.h
@@ -0,0 +1,64 @@
+#ifndef SERVICE_H
+#define SERVICE_H
+
+#include <QLocalServer>
+#include <QLocalSocket>
+#include <QStringList>
+#include <QMap>
+#include "improtocol.h"
+
+class Msg
+{
+ /*
+    service to client message protocol:
+    "<msg protocol=irc1 app=messagingclient threadId=1>this is a message</msg>"
+    "<msg protocol=IMServer app=registerService name=messagingclient status=online></msg>
+
+   */
+    public:
+        Msg(QByteArray msg);
+        QString& operator [](QString& name);
+    private:
+        QMap<QString, QString> items;
+
+};
+
+class IMClient:public QLocalSocket
+{
+    Q_OBJECT
+    public:
+        IMClient(){}
+        ~IMClient(){}
+        QString name;
+};
+
+class IMServerManager:public QObject
+{
+    Q_OBJECT
+    public:
+        IMServerManager();
+        ~IMServerManager();
+    protected:
+        void processClientRequest(IMClient* client, Msg& msg);
+        void registerClient(QString appName, IMClient* client);
+
+class IMService: public IMServerManager
+{
+    Q_OBJECT
+    public:
+        IMService();
+        ~IMService();
+        bool startProtocol(QString& name);
+        bool stopProtocol(QString& name);
+    private:
+        QLocalServer d_server;
+        QList<IMClient*> clients;
+        QList<IMProtocol*> protocols;
+    private slots:
+        void newConnection();
+        void clientMsg();
+        void protocolMsg();
+};
+
+#endif
+
