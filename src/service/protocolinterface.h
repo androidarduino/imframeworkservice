dc79b0ac01e6d8cfb193472e7e4c10267aa7bac2
vrcats <vrcats@79cadf2e-45d3-11de-a1df-459bf3fe8569>
2012-05-04 05:41:53 +0000

    modified the plugin system, now compiles, but not fully working

--- src/service/protocolinterface.h
+++ src/service/protocolinterface.h
@@ -4,30 +4,32 @@
 #include <QString>
 #include <QList>
 #include "../msg.h"
+#include <QtPlugin>
 
 /*
-    Abstract of the protocols, used to load/unload the protocol instances, pass it to the server, and send messages for server.
+   Abstract of the protocols, used to load/unload the protocol plugin instances, pass it to the server, and send messages for server.
    */
 
-class IMProtocol:public QObject
+class IMProtocol
 {
-	Q_OBJECT
-	public:
-		virtual IMProtocol(Msg& parameters);
-		virtual ~IMProtocol();
-        virtual bool available();
-		virtual QList<Msg> onlineBuddies();
-		virtual QString& operator [](QString propertyName);
-	signals:
-		virtual void msgArrived(Msg& msg);
-		virtual void statusChanged(QString status);
-	public slots:
-		virtual void sendMsg(Msg& msg);
-		virtual void login();
+    public:
+        IMProtocol(){}
+        virtual ~IMProtocol(){}
+        virtual bool available()=0;
+        virtual QList<Msg> onlineBuddies()=0;
+        virtual QString& operator [](QString propertyName)=0;
+/*
+    signals:
+        virtual void msgArrived(Msg& msg)=0;
+        virtual void statusChanged(QString status)=0;
+*/
+    public:
+        virtual void sendMsg(Msg& msg)=0;
+        virtual void login()=0;
     private:
         Msg d_properties;
 };
 
-Q_DECLARE_INTERFACE(IMProtocol, "com.vrcats.IMFramework.IMProtocol");
+Q_DECLARE_INTERFACE(IMProtocol, "com.vrcats.IMFramework.IMProtocol/1.0");
 
 #endif
