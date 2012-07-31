44dbd3e9c61e81aa89a2f37e41b93c10305238f8
vrcats <vrcats@79cadf2e-45d3-11de-a1df-459bf3fe8569>
2012-07-31 14:30:51 +0000

    irc protocol implemented but not tested yet

---
+++ src/tests/main.cpp
@@ -0,0 +1,9 @@
+/*
+	This is a test client file that does the following things;
+
+	1. register a service
+	2. send a message
+	3. query buddy status
+	4. pretend a service failure
+	5. receive and dispatch service
+*/
