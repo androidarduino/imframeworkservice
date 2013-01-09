b7cdac80a3810d1f1f84d10fb92de7f4552390f2
vrcats <vrcats@79cadf2e-45d3-11de-a1df-459bf3fe8569>
2013-01-09 11:12:21 +0000

    compiling

--- src/protocols/irc/ircplugin.cpp
+++ src/protocols/irc/ircplugin.cpp
@@ -1,8 +1,76 @@
 #include "ircplugin.h"
 
+IMIRCPlugin::IMIRCPlugin()
+{
+//initialize an irc client
+	d_client=new IRCClient("", 0);
+	connect(d_client, SIGNAL(message(QString, QString, QString, QString)), this, SLOT(gotMsg(QString, QString, QString, QString))); 
+}
+IMIRCPlugin::~IMIRCPlugin()
+{
+//destroy the irc client
+	d_client->disconnect();
+}
+bool IMIRCPlugin::available()
+{
+//return whether the client is available
+	return d_client->d_connected;
+}
+QString& IMIRCPlugin::operator[](QString ref)
+{
+//return parameters of the client
+	ref=ref.toLower();
+	if(ref=="username")
+		return d_client->d_userName;
+	if(ref=="status")
+		return d_client->d_status;
+	if(ref=="realname")
+		return d_client->d_realName;
+	return *(new QString(""));
+}
+void IMIRCPlugin::sendMsg(Msg& msg)
+{
+//send message using options in msg
+	QString user=msg["user"];
+	QString message=msg["body"];	
+	d_client->send(user, message);
+}
 void IMIRCPlugin::login()
 {
     qDebug()<<"irc logging in...";
+	d_client->login();
+}
+QList<Msg> IMIRCPlugin::onlineBuddies()
+{
+//return online users, including channels and online users
+	QList<Msg>& ret=*(new QList<Msg>());
+	foreach(QString c, d_client->channels())
+	{
+		Msg *m=new Msg();
+		(*m)["name"]=c;
+		(*m)["uri"]=c+"_channel@"+d_client->d_server;
+		(*m)["type"]="group";
+		ret<<*m;
+	}
+	foreach(QString c, d_client->users())
+	{
+		Msg *m=new Msg();
+		(*m)["name"]=c;
+		(*m)["uri"]=c+"@"+d_client->d_server;
+		(*m)["type"]="user";
+		ret<<*m;
+	}
+	return ret;
 }
 
+void IMIRCPlugin::gotMsg(QString from, QString fromUri, QString receiver, QString msg)
+{
+//called when a message is received, emit signal
+	Msg& m=*(new Msg());
+	m["from"]=from;
+	m["fromUri"]=fromUri;
+	m["receiver"]=receiver;
+	m["msg"]=msg;
+	emit(msgArrived(m));
+}
 Q_EXPORT_PLUGIN2(imircplugin, IMIRCPlugin);
