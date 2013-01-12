221356c15f983a5841995fb7129bf28e5c4fbf5e
vrcats <vrcats@79cadf2e-45d3-11de-a1df-459bf3fe8569>
2013-01-12 04:51:44 +0000

    abc

--- trunk/src/common/protocolinterface.h
+++ trunk/src/common/protocolinterface.h
@@ -12,23 +12,27 @@
 
 class IMProtocol
 {
-    public:
-        IMProtocol(){}
-        virtual void init(Msg& config)=0;
-        virtual ~IMProtocol(){}
-        virtual bool available()=0;
-        virtual QList<Msg> onlineBuddies()=0;
-        virtual QString& operator [](QString propertyName)=0;
-    signals:
-        virtual void msgArrived(Msg& msg)=0;
-        virtual void statusChanged(QString status)=0;
-    public:
-        virtual void sendMsg(Msg& msg)=0;
-        virtual void login()=0;
-    private:
-        Msg d_properties;
+public:
+  IMProtocol ()
+  {
+  }
+  virtual void init (Msg & config) = 0;
+  virtual ~ IMProtocol ()
+  {
+  }
+  virtual bool available () = 0;
+  virtual QList < Msg > onlineBuddies () = 0;
+  virtual QString & operator [] (QString propertyName) = 0;
+signals:
+  virtual void msgArrived (Msg & msg) = 0;
+  virtual void statusChanged (QString status) = 0;
+public:
+  virtual void sendMsg (Msg & msg) = 0;
+  virtual void login () = 0;
+private:
+  Msg d_properties;
 };
 
-Q_DECLARE_INTERFACE(IMProtocol, "com.vrcats.IMFramework.IMProtocol/1.0");
+Q_DECLARE_INTERFACE (IMProtocol, "com.vrcats.IMFramework.IMProtocol/1.0");
 
 #endif
