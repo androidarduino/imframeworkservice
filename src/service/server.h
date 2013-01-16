55938ad16e3244e27a8e6b6cc73af0a2cca5ab9c
vrcats <vrcats@79cadf2e-45d3-11de-a1df-459bf3fe8569>
2013-01-16 21:34:54 +0000

    restructured plugin and server, created todo list

--- src/service/server.h
+++ src/service/server.h
@@ -16,58 +16,62 @@
 
 /*
    Abstract of the client delegation, talk to the client program on behalf of the server.
-   */
+ */
 class IMClient:public QObject
 {
-    Q_OBJECT
-    public:
-        IMClient(QLocalSocket* socket);
-        ~IMClient(){}
-        QString name;
-	QLocalSocket* d_socket;
+	Q_OBJECT
+	public:
+		IMClient(QLocalSocket* socket);
+		~IMClient(){}
+		QString name;
+		QLocalSocket* d_socket;
 };
 
 /*
    Base class of the server, process some server sepecific matters, like register/unregister clients, etc.
-*/
+ */
 class IMServerManager:public QObject//, public IMProtocol
 {
-    Q_OBJECT
-    //Q_INTERFACES(IMProtocol)
-    public:
-        IMServerManager();
-        ~IMServerManager();
-    protected:
-        void processClientRequest(IMClient* client, Msg& msg);
-        void registerClient(QString appName, IMClient* client);
-    public:
-        bool available();
-        QList<Msg> onlineBuddies();
-        QString& operator [](QString propertyName);
-    public slots:
-        void sendMsg(Msg& msg);
-        void login();
+	Q_OBJECT
+		//Q_INTERFACES(IMProtocol)
+	public:
+		IMServerManager();
+		~IMServerManager();
+	protected:
+		void processClientRequest(IMClient* client, Msg& msg);
+		void registerClient(QString appName, IMClient* client);
+	public:
+		bool available();
+		QList<Msg> onlineBuddies();
+		QString& operator [](QString propertyName);
+		public slots:
+			void sendMsg(Msg& msg);
+		void login();//may be unuseful
+		void pluginMsg(Msg& msg);//called when received a msg from plugin
+	protected:
+		void dispatchMsg(Msg& msg);//helper function for pluginMsg();
 };
 
 /*
    Main part of the server
-*/
+ */
 class IMService: public IMServerManager
 {
-    Q_OBJECT
-    public:
-        IMService();
-        ~IMService();
-        bool controlProtocol(QString& name, QString& command);
-    private:
-        QLocalServer d_server;
-        QList<IMClient*> d_clients;
-        QList<IMProtocol*> d_protocols;
-        bool loadPlugins();
-    private slots:
-        void newConnection();
-        void clientMsg();
-        void protocolMsg(Msg& msg);
+	Q_OBJECT
+	public:
+		IMService();
+		~IMService();
+		bool controlProtocol(QString& name, QString& command);
+	private:
+		QLocalServer d_server;
+		QList<IMClient*> d_clients;
+		QList<IMProtocol*> d_protocols;
+		bool loadPlugins();
+		bool unloadPlugins();
+		private slots:
+			void newConnection();
+		void clientMsg();
+		void protocolMsg(Msg& msg);
 };
 
 #endif
